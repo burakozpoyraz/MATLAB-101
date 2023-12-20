552a3f58dcaaf20b236f01411c25b6c8fead5f45
Burak Özpoyraz <burakozpoyraz@gmail.com>
2023-12-20 15:17:38 +0100

    Lecture4 content update

--- Lecture 4/Coding Documents/MyRecFactorial.m
+++
@@ -1,9 +0,0 @@
-function f = MyRecFactorial(x)
-    if x < 0
-        error("Please enter a non-negative integer.");
-    elseif x == 0
-        f = 1;
-    else
-        f = x * MyRecFactorial(x - 1);
-    end
-end
